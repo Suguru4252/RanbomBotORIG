4ebc77e82b755caeabc298db1e98cd4609f26d82
Suguru4252 <suguruartem@gmail.com>
2026-03-06 17:52:48 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ from aiogram.exceptions import TelegramBadRequest
 # ==================== НАСТРОЙКИ ====================
 
 # Токен бота (получи у @BotFather)
-BOT_TOKEN = "8572906701:AAFpWLGbEZqvZsupPZqElr0q197f3WllvYU"  # <--- ЗАМЕНИ НА СВОЙ ТОКЕН!
+BOT_TOKEN = "8703891124:AAFp8QR42TeKijiEMEOG5NUZEQaylokDDwU"  # <--- ЗАМЕНИ НА СВОЙ ТОКЕН!
 
 # ID администратора (для уведомлений)
 ADMIN_ID = 5596589260  # <--- ЗАМЕНИ НА СВОЙ ID (узнай у @userinfobot)
